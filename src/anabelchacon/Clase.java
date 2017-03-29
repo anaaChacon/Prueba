3ae259dba2469c5b74f504768b20257837d60085
QW\Anabel <anaachacon@hotmail.com>
2017-03-29 22:33:51 +0200

    Prueba1

---
+++ src/anabelchacon/Clase.java
@@ -0,0 +1,13 @@
+package anabelchacon;
+import javax.swing.JOptionPane;
+
+public class Clase extends JOptionPane{
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+  
+  showMessageDialog(null, "Esa peñita guapa", "Titulo", JOptionPane.INFORMATION_MESSAGE);
+
+ }
+
+}
